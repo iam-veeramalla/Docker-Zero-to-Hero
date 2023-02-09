bdaeeb1aaf5076508a8ae08b2a8a139a278775aa
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2023-02-09 16:49:41 +0530

    Update settings.py

--- examples/python-web-app/devops/devops/settings.py
+++ examples/python-web-app/devops/devops/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'devops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'demo/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
